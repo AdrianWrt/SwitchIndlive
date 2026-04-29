112bc329335047a06b32383efdbffb30b0524625
ian <adrwartono@gmail.com>
2026-04-29 19:53:43 +0700

    fixes

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,25 +1,35 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: NextRequest) {
-  const search = req.nextUrl.searchParams.get("search") || "";
+  try {
+    const search = req.nextUrl.searchParams.get("search") || "";
 
-  const products = await prisma.product.findMany({
-    where: {
-      OR: [
-        {
-          name: {
-            contains: search,
-          },
-        },
-        {
-          description: {
-            contains: search,
-          },
-        },
-      ],
-    },
-  });
+    const products = await prisma.product.findMany({
+      where: search
+        ? {
+            OR: [
+              {
+                name: {
+                  contains: search,
+                },
+              },
+              {
+                description: {
+                  contains: search,
+                },
+              },
+            ],
+          }
+        : {},
+    });
 
-  return NextResponse.json({ products });
+    return NextResponse.json({ products });
+  } catch (err) {
+    console.error("PRODUCTS API ERROR:", err);
+
+    return NextResponse.json({ products: [] });
+  }
 }
