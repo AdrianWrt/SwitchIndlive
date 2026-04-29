d306fde5708db645369366039c2b9c1451f1ebc7
ian <adrwartono@gmail.com>
2026-04-30 00:19:30 +0700

    fixes

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -1,23 +1,13 @@
-import NextAuth from "next-auth";
+import "next-auth";
 
 declare module "next-auth" {
-  interface Session {
-    user: {
-      id?: string;
-      role?: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
-  }
-
   interface User {
     role?: string;
   }
-}
 
-declare module "next-auth/jwt" {
-  interface JWT {
-    role?: string;
+  interface Session {
+    user: {
+      role?: string;
+    } & DefaultSession["user"];
   }
 }
