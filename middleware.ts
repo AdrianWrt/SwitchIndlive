fb430dd2be80a917103ddc3dc3de9c613be9c0a6
ian <adrwartono@gmail.com>
2026-04-29 21:54:32 +0700

    fixes

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,6 @@
-import { auth } from "@/auth";
-
-export default auth((req: any) => {
-  try {
-    const { pathname } = req.nextUrl;
-
-    if (
-      pathname.startsWith("/api") ||
-      pathname.startsWith("/login") ||
-      pathname.startsWith("/register") ||
-      pathname.startsWith("/_next") ||
-      pathname === "/"
-    ) {
-      return;
-    }
-
-    if (!req.auth) {
-      const url = new URL("/login", req.url);
-      return Response.redirect(url);
-    }
-  } catch (err) {
-    console.error("MIDDLEWARE ERROR:", err);
-    return;
-  }
-});
+export default function middleware() {
+  return;
+}
 
 export const config = {
   matcher: ["/((?!_next|favicon.ico).*)"],
